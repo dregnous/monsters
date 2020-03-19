7ce4491cd7cdc493b0520ce509fd1d0711d06414
Vishwajeet Kumbhakar <vishwajeet6kumbhakar@gmail.com>
2020-03-19 16:12:15 +0530

    Updates

---
+++ precache-manifest.2ff844cdd596bc8747685befc4ac45f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac80fac7991810f54066fbc331fe08ad",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "71706b3a4c23dfbe3d0e",
+    "url": "/monsters/static/css/main.b9f6a5a5.chunk.css"
+  },
+  {
+    "revision": "0d35040140e440a26978",
+    "url": "/monsters/static/js/2.1313f230.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters/static/js/2.1313f230.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71706b3a4c23dfbe3d0e",
+    "url": "/monsters/static/js/main.2450f318.chunk.js"
+  },
+  {
+    "revision": "8d91a7365af0235ec77f",
+    "url": "/monsters/static/js/runtime-main.0046601a.js"
+  }
+]);
